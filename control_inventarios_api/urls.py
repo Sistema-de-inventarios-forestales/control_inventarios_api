8fb0508888548fac2e4678926dfdf04609d64982
Guido Cruz <guido_cruz_huanca@outlook.com>
2022-03-08 14:29:02 -0400

    hangeseted to paths

---
+++ control_inventarios_api/urls.py
@@ -0,0 +1,8 @@
+# control_inventarios/urls.py
+from django.urls import path
+from .views import HomePageView, AboutPageView
+
+urlpatterns = [
+    path("", HomePageView.as_view(), name="inventarios"),
+    path("about", AboutPageView.as_view(), name="about"),
+]
--- control_inventarios_api/views.py
+++ control_inventarios_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+# control_inventarios/views.py
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "inventarios.html"
+
+
+class AboutPageView(TemplateView):  # new
+    template_name = "about.html"
